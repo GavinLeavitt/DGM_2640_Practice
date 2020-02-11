94e9b33edf57c67c3180ae147dd21f6deaefbfb8
Unknown <gavin.leavitt@gmail.com>
2020-02-10 23:30:23 -0700

    Work on magnets and movement scripts

---
+++ EMF Project/Assets/Scripts/PositionData.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class PositionData : ScriptableObject {
+	public Vector3 positionVector;
+}
--- EMF Project/Assets/Scripts/RotateTransform.cs
+++ EMF Project/Assets/Scripts/RotateTransform.cs
@@ -6,15 +6,19 @@ using UnityEngine;
 public class RotateTransform : MonoBehaviour {
 
 	public Transform transformToRotate;
+	public string cwKey = "x";
+	public string ccwKey = "z";
+	
 	void Start () {
 		transformToRotate = GetComponent<Transform>();
 	}
+	
 	void Update () {
-		if (Input.GetKey("q")) {
+		if (Input.GetKey(ccwKey)) {
 			transformToRotate.Rotate(0.0f, -2.0f, 0.0f);
 		}
 
-		if (Input.GetKey("e")) {
+		if (Input.GetKey(cwKey)) {
 			transformToRotate.Rotate(0.0f, 2.0f, 0.0f);
 		}
 	}
