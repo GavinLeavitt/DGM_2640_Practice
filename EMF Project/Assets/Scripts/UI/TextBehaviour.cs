38f63929c249ae3f87a72e0b7897179436e1b84a
Gavin Leavitt <gavin.leavitt@gmail.com>
2020-03-03 13:09:38 -0700

    StringList Data and Text object behavior

---
+++ EMF Project/Assets/Scripts/UI/TextBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class TextBehaviour : MonoBehaviour
+{
+    private Text textObj;
+    public StringListData stringListDataObj;
+    private void Start()
+    {
+        textObj = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        textObj.text = stringListDataObj.ReturnCurrentLine();
+    }
+}
