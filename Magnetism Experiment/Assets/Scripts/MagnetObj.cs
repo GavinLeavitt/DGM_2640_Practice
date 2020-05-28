0e7ac7d5690293f88a8f164bd30d00202ea3c5fb
Unknown <gavin.leavitt@gmail.com>
2020-05-27 19:02:48 -0600

    Physics-based experiment with too many dependencies

---
+++ Magnetism Experiment/Assets/Scripts/MagnetObj.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class MagnetObj : MonoBehaviour {
+	public float charge = 1.0f;
+	public Rigidbody rigidbodyObj;
+
+	public void ChangePolarity(float newPolarity)
+	{
+		charge = newPolarity;
+	}
+}
---
+++ Magnetism Experiment/Assets/Scripts/Movement Behaviours/MagnetizeBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MagnetObj))]
+public class MagnetizeBehaviour : MonoBehaviour {
+
+	public MagnetObj magnetObj;
+	public float constForce = 80f;
+	
+	public void Awake()
+	{
+		magnetObj = GetComponent<MagnetObj>();
+	}
+
+	public void Magnetize(MagnetObj targetMagnet)
+	{
+		var thisPosition = magnetObj.rigidbodyObj.position;
+		var targetPosition = targetMagnet.rigidbodyObj.position;
+		var distanceBetweenPositions = Vector3.Distance(targetPosition, thisPosition);
+		Debug.Log(distanceBetweenPositions);
+		var vectorBetweenPositions = Vector3.Normalize(thisPosition - targetPosition);
+		Debug.Log(vectorBetweenPositions);
+		// Apply force according to Coulomb's law
+		magnetObj.rigidbodyObj.AddForce(constForce * ((magnetObj.charge * targetMagnet.charge) / Mathf.Pow(Mathf.Abs(distanceBetweenPositions), 2.0f)) * vectorBetweenPositions);
+	}
+}
