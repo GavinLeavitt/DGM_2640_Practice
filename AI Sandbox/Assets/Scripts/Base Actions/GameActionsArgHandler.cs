b2dfba226510b9ab9d8507efca628332541723ff
Unknown <gavin.leavitt@gmail.com>
2020-07-06 12:38:04 -0600

    Implemented Anthony's AIHunt, more AI experiments

---
+++ AI Sandbox/Assets/Scripts/Base Actions/GameActionsArgHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameActionsArgHandler : MonoBehaviour
+{
+    public UnityEvent startEvent;
+
+    private void Start()
+    {
+        startEvent.Invoke();
+    }
+
+    [Serializable]
+    public struct Handlers
+    {
+        public GameAction action;
+		public UnityEvent<object> actionEvent;
+        
+        internal void Respond(object eventObj)
+        {
+            actionEvent.Invoke(eventObj);
+        }
+    }
+    
+    public List<Handlers> handlerList;
+    
+    private void OnEnable()
+    {
+        foreach (var obj in handlerList)
+        {
+            obj.action.raise += obj.Respond;
+        }
+    }
+}
---
+++ EMF Composition/Assets/Scripts/Movement Behaviours/ParentBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParentBehaviour : MonoBehaviour {
+	public GameObject gameObj;
+
+	public void OnTriggerEnter(Collider other)
+	{
+		if (other.tag != "Pickup") return;
+		other.transform.parent = gameObj.transform;
+		other.transform.localPosition = new Vector3(0, 0, 0);
+	}
+
+	public void OnTriggerExit(Collider other)
+	{
+		if (other.tag != "Pickup") return;
+		other.transform.parent = null;
+	}
+
+	public void SetParent(GameObject newParent)
+	{
+		gameObj.transform.parent = newParent.transform;
+		Debug.Log("New Parent: " + gameObj.transform.parent.name);
+
+		if (newParent.transform.parent != null)
+		{
+			Debug.Log("New grandparent: " + gameObj.transform.parent.parent.name);
+		}
+	}
+
+	public void DetachFromParent()
+	{
+		gameObj.transform.parent = null;
+	}
+}
